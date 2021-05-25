0fd15889a40a16a8ce4bad42154ee34b5a009df8
Jordan Fuller <jrdnstoltz+mountainsine@gmail.com>
2021-05-25 08:54:33 -0500

    added monster array

--- main.js
+++ main.js
@@ -4,11 +4,31 @@
 const KAIJU = [
     { name: 'Godzilla', tier: 1 },
     { name: 'King Kong', tier: 2 },
-    { name: 'Gamera', tier: 3 },
+    { name: 'Gamera', tier: 1 },
     { name: 'Mothra', tier: 4 },
+    { name: 'Ghidora', tier: 4 },
+    { name: 'Ultraman', tier: 1 },
+    { name: 'Rodan', tier: 4 },
     ]
 
-    KAIJU.forEach((monster) => {
-    console.log(monster.name)
+var monsterVariable = KAIJU((monster) => {
+    //console.log(monster.name)
+     push(monster.name);
+    //console.log(session)
     }
 )
+
+// Creating an example of JSON
+
+  
+  // Converting the JSON string with JSON.stringify()
+  // then saving with localStorage in the name of session
+  //localStorage.setItem('session', JSON.stringify(session));
+  
+  // Example of how to transform the String generated through
+  // JSON.stringify() and saved in localStorage in JSON object again
+  //var restoredSession = JSON.parse(localStorage.getItem('session'));
+  
+  // Now restoredSession variable contains the object that was saved
+  // in localStorage
+  //console.log(restoredSession);
