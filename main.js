fa491e49a8847d5f8248e172b02c60d5fa2c9a66
Jordan Fuller <jrdnstoltz+mountainsine@gmail.com>
2021-05-24 21:00:48 -0500

    array example js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-    alert( "Hello, world!" );
+    // alert( "Hello, world!" );
+
+    // Create an array of Movie monsters
+const KAIJU = [
+    { name: 'Godzilla', tier: 1 },
+    { name: 'King Kong', tier: 2 },
+    { name: 'Gamera', tier: 3 },
+    { name: 'Mothra', tier: 4 },
+    ]
+
+    KAIJU.forEach((monster) => {
+    console.log(monster.name)
+    }
+)
