b15988e22a72b1d119b1bea3411d15e81831f6ea
Jordan Fuller <jrdnstoltz+mountainsine@gmail.com>
2021-06-02 10:36:57 -0500

    cleanup

--- main.js
+++ main.js
@@ -11,27 +11,9 @@ const KAIJU = [
     { name: 'Rodan', tier: 4 },
     ]
 
-var monsterVariable = KAIJU((monster) => {
-    //console.log(monster.name)
-     push(monster.name);
-    //console.log(session)
-    }
-)
-
-// Creating an example of JSON
-
-  
-  // Converting the JSON string with JSON.stringify()
-  // then saving with localStorage in the name of session
-  //localStorage.setItem('session', JSON.stringify(session));
-  
-  // Example of how to transform the String generated through
-  // JSON.stringify() and saved in localStorage in JSON object again
-  //var restoredSession = JSON.parse(localStorage.getItem('session'));
-  
-  // Now restoredSession variable contains the object that was saved
-  // in localStorage
-  //console.log(restoredSession);
+    console.log(monster.name)
+    push(monster.name);
+;
 
 
   
