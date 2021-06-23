09be35ad2d8b302ce99de1c2617e1372a7b5473b
Jordan Fuller <jrdnstoltz+mountainsine@gmail.com>
2021-06-22 21:13:31 -0500

    updates

---
+++ combos.js
@@ -0,0 +1,33 @@
+    // alert( "Hello, world!" );
+
+
+    // Create an array of decks with a tier property
+const decks = [
+    { name: 'The Warmaster that Stunts on Gravity', tier: 0 },
+    { name: 'Bottomgold, the Officer of the Saloon', tier: 0 },
+    { name: 'Paimon, Lancaiars Traveller', tier: 0 },
+    { name: 'The Empress of Halesarth', tier: 0 },
+    { name: 'E. Burdett, the Manor Exorcist', tier: 0 },
+    { name: 'Benefucious O. Termineye, the Fifth', tier: 0 },
+    { name: 'I. Tolstoy, the Orbit Philosopher', tier: 0 },
+    { name: 'Frau Alphacell Tenebris', tier: 0 },
+    { name: 'The Song that Tumultuously Betrayed Keys', tier: 0 },
+    { name: 'The Thwartive Chancellor', tier: 0 },
+    { name: 'Mentor “Parallel” Praesentius', tier: 0 },
+    { name: 'Pan Vapidhursts Musician', tier: 0 },
+    ]
+
+    const marscombos = [
+        { name: 'Battle Fleet/Key Abduction', tier: 0 },
+        { name: 'Martian Generosity/Key Abduction', tier: 0 },
+        { name: 'Commpod/Crystal Hive', tier: 0 },
+        { name: 'Grommid/Swap Widget', tier: 0 },
+        { name: 'Total Recall / Glorious few', tier: 0 },
+        { name: 'Hypno Beam / Glyxox Proliferator', tier: 0 },
+        { name: 'theyre everywhere / mars needs aember', tier: 0 },
+        { name: 'Frau Alphacell Tenebris', tier: 0 },
+        { name: 'The Song that Tumultuously Betrayed Keys', tier: 0 },
+        { name: 'The Thwartive Chancellor', tier: 0 },
+        { name: 'Mentor “Parallel” Praesentius', tier: 0 },
+        { name: 'Pan Vapidhursts Musician', tier: 0 },
+        ]
