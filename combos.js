61dd7eaf4b8b24e0c9de0f7823e0e3cb0458492e
Jordan Fuller <jrdnstoltz+mountainsine@gmail.com>
2021-06-25 08:29:00 -0500

    updates

--- combos.js
+++ combos.js
@@ -1,33 +1,136 @@
-    // alert( "Hello, world!" );
 
-
-    // Create an array of decks with a tier property
-const decks = [
-    { name: 'The Warmaster that Stunts on Gravity', tier: 0 },
-    { name: 'Bottomgold, the Officer of the Saloon', tier: 0 },
-    { name: 'Paimon, Lancaiars Traveller', tier: 0 },
-    { name: 'The Empress of Halesarth', tier: 0 },
-    { name: 'E. Burdett, the Manor Exorcist', tier: 0 },
-    { name: 'Benefucious O. Termineye, the Fifth', tier: 0 },
-    { name: 'I. Tolstoy, the Orbit Philosopher', tier: 0 },
-    { name: 'Frau Alphacell Tenebris', tier: 0 },
-    { name: 'The Song that Tumultuously Betrayed Keys', tier: 0 },
-    { name: 'The Thwartive Chancellor', tier: 0 },
-    { name: 'Mentor “Parallel” Praesentius', tier: 0 },
-    { name: 'Pan Vapidhursts Musician', tier: 0 },
+// TEMPLATE ARRAY
+const combostemplate = [
+    { name: 'NAME', notes: '', own:''},
     ]
 
-    const marscombos = [
-        { name: 'Battle Fleet/Key Abduction', tier: 0 },
-        { name: 'Martian Generosity/Key Abduction', tier: 0 },
-        { name: 'Commpod/Crystal Hive', tier: 0 },
-        { name: 'Grommid/Swap Widget', tier: 0 },
-        { name: 'Total Recall / Glorious few', tier: 0 },
-        { name: 'Hypno Beam / Glyxox Proliferator', tier: 0 },
-        { name: 'theyre everywhere / mars needs aember', tier: 0 },
-        { name: 'Frau Alphacell Tenebris', tier: 0 },
-        { name: 'The Song that Tumultuously Betrayed Keys', tier: 0 },
-        { name: 'The Thwartive Chancellor', tier: 0 },
-        { name: 'Mentor “Parallel” Praesentius', tier: 0 },
-        { name: 'Pan Vapidhursts Musician', tier: 0 },
-        ]
+// GENERAL COMBOS
+const combosgeneral = [
+    { name: 'Control the Weak / A fair Game', notes: '', own},
+    ]
+
+// MARS COMBOS
+const marscombos = [
+    { name: 'Battle Fleet / Key Abduction', notes: '', own:''},
+    { name: 'Battle Fleet / Incubation Chamber', notes: '', own:''},
+    { name: 'Martian Generosity / Key Abduction', notes: '', own:''},
+    { name: 'Martian Generosity / Key Abduction / Nepenthe seed', notes: '', own:''},
+    { name: 'Martian Generosity / Total Recall', notes: '', own:''},
+    { name: 'Martian Generosity / Key Abduction', notes: '', own:'2'},
+    { name: 'Martian Generosity / Glyxl Proliferator', notes: '', own:''},
+    { name: 'Martian Generosity / x2 Key Abduction', notes: '', own:'1'},
+    { name: 'Martian Generosity / Might Makes Right', notes: '', own:''},
+    { name: 'CUSTOM VIRUS / SANCTUM OR LOGOS', notes: '', own:''},
+    { name: 'Glyxl proliferator /  Mars first', notes: '', own:''},
+    { name: 'Glyxl proliferator / Invasion Portal', notes: '', own:''},
+    { name: 'Glyxl Proliferator / Hypno Beam', notes: '', own:''},
+    { name: 'theyre everywhere / Mars Needs Aember', notes: '', own:''},
+    { name: 'Total Recall / Glorious few', notes: '', own:''},
+    { name: 'Glyxl proliferator / Hypno Beam', notes: '', own:'5'},
+    { name: 'Extinction / Phloxem Spike', notes: '', own:''},
+    { name: 'Commpod / Crystal Hive', notes: '', own:''},
+    { name: 'john Smyth / Megamouth', notes: '', own:''},
+    { name: 'john Smyth / Zookeeper', notes: '', own:''},
+    { name: 'Total Recall / Glorious Few', notes: '', own:''},
+    { name: 'Mars Needs Aember / Theyre Everywhere', notes: '', own:''},
+    { name: 'Biomatrix backup / Drumble', notes: '', own:''},
+    { name: 'Biomatrix backup / Dust Pixie', notes: '', own:''},
+    { name: 'Combat Pheromones / Crystal Hive', notes: '', own:''},
+    { name: 'Collector Worm / Way of the Bear', notes: '', own:''},
+    { name: 'Glyxl Proliferator / Duskwitch', notes: '', own:''},
+    { name: 'Skybooster Squadron / Duskwitch', notes: '', own:''},
+    { name: 'Beam Buckler / Mars Needs Aember', notes: '', own:''},
+    { name: 'Incubation chamber / Hypnotic command', notes: '', own:''},
+    { name: 'Grommid / Swap Widget', notes: '', own:''},
+    { name: 'Grommid / Exterminate Exterminate', notes: '', own:''},
+    { name: 'Grommid / Pawn Sacrifice', notes: '', own:''},
+    ]
+
+// DIS COMBOS
+const discombos = [
+    { name: 'Gateway / Arise', notes: '', own:''},
+    { name: 'Drumble / Snudge', notes: '', own:''},
+    { name: 'Multipl Mind Fires', notes: '', own:''},
+    { name: 'Essence Scale / Mulitple Q Mechs', notes: '', own:''},
+    { name: 'Pitlord / Hecatomb', notes: '', own:''},
+    { name: 'Buzzle / Collar Of Subordination', notes: '', own:''},
+    { name: 'Punctuated Equilibrium / E on the fringes', notes: '', own:''},
+    { name: 'Exhume / Resteringuntus', notes: '', own:''},
+    { name: 'Exhume / Ronnie Wristclocks', notes: '', own:''},
+    { name: 'Mind barb / Screaming Cave', notes: '', own:''},
+    { name: 'Arise / Resteringuntus', notes: '', own:''},
+    { name: 'Arise / multiple ember imp', notes: '', own:''},
+    { name: 'Control the Weak + A Fair Game', notes: '', own:''},
+    { name: 'Annihilation Ritual / Screaming Cave', notes: '', own:''},
+    { name: 'Mind Barb / Screaming Cave', notes: '', own:''},
+    { name: 'Hectatomb / x3 dust imp', notes: '', own:''},
+    { name: 'Gleeful Mayhem / Harbringer of doom', notes: '', own:''},
+    { name: 'Screaming Cave / x2  Control the Weak', notes: '', own:''},
+    { name: 'Screaming Cave / x3 mind barb', notes: '', own:''},
+    { name: 'Drumble / Arise', notes: '', own:''},
+    { name: 'Drumble / The Terror', notes: '', own:''},
+    { name: 'Hysteria / Fair Game ', notes: '', own:''},
+    { name: 'Restriguntus / Shadow Self', notes: '', own:''},
+    { name: 'Pandemonium / Guilty Hearts', notes: '', own:''},
+    { name: 'Ember Imp / Flame Wreathed', notes: '', own:''},
+    { name: 'Dust Imp / Old Yurk', notes: '', own:''},
+    { name: 'Lord Invidius / Essence Scale', notes: '', own:''},
+    { name: 'Pitlord / Wretched Doll', notes: '', own:''},
+    { name: 'Infurnace / Sci officer Morpheus', notes: '', own:''},
+    { name: 'Infurnace / Hysteria', notes: '', own:''},
+    { name: 'x2 Harvest Time', notes: '', own:''},
+    { name: 'Treuburu / Sinder', notes: '', own:''},
+    { name: 'DRUMBLE + HECATOMB', notes: '', own:''},
+    { name: 'MIND FIRE + SCREAMING CAVE', notes: '', own:''},
+    { name: 'GRIM REMINDER / KIRBY', notes: '', own:''},
+    { name: 'EXUME  / QUANT', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+
+    ]
+
+// SHADOWS COMBOS
+const shadowscombos = [
+    { name: 'Sinestra/Scowly Caper', tier: 0 },
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    ]
+
+
+// UNTAMED COMBOS
+const untamedcombos = [
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    ]
+
+// BROBNAR COMBOS
+const brobnarcombos = [
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    ]
+
+// SANCTUM COMBOS
+const sanctumcombos = [
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    ]
+
+// UNFATHOMABLE COMBOS
+const unfathomablecombos = [
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    { name: 'NAME', notes: '', own:''},
+    ]
+
