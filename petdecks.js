ae7bdad5e358c494a1428b27c58fe5889fb16a6d
Jordan Fuller <jrdnstoltz+mountainsine@gmail.com>
2021-08-23 19:30:39 -0500

    update

--- petdecks.js
+++ petdecks.js
@@ -1,19 +1,20 @@
 window.addEventListener("load", function(){
 
     var petdecks = [
-      "Pan, Vapidhurst's Musician", 
-      "Eva, the Justiciar of Godwillow", 
-      "Xtraveo of the Hunter's Canyon", 
-      ];
-  
+      { name: "Pan, Vapidhurst's Musician", tier: 1},
+      { name: "Eva, the Justiciar of Godwillow", tier: 1 },
+      { name: "Xtraveo of the Hunter's Canyon",  tier: 1 },
+    ]
+
     // (B) CREATE HTML TABLE STRING
     var perrow = 2, // 2 CELLS PER ROW
         html = "<table> <th>DECKS</th><th>OWN?</th><tr>";
   
     // LOOP THROUGH ARRAY AND ADD TABLE CELLS
     for (var i=0; i<petdecks.length; i++) {
+      petdecks = petdecks.values(this.name);
       // "NORMAL" CELL
-      html += `<td>${petdecks[i]}</td>`;
+      html += `<td>${petdecks[i]}</td><tr><tr>`;
   
       // CLICK ON CELL TO DO SOMETHING 
       // html += `<td onclick="FUNCTION()">${petdecks[i]}</td>`;
